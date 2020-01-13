d07e998a0bbb7739c166611cb9ee34176d338fa5
ZSSXL <1271130458@qq.com>
2020-01-13 17:34:35 +0800

    merge

--- ArrangeProduceClient/js/app-url.js
+++ ArrangeProduceClient/js/app-url.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 /*const serverUrl = "http://192.168.1.110:8080";
 const websocketUrl = "ws://192.168.1.110:8080";*/
-=======
-/*const serverUrl = "http://127.0.0.1:8080";
-const websocketUrl = "ws://127.0.0.1:8080";*/
->>>>>>> df0f202e053adb16c7c583806beea88fb09345e5
 
 const serverUrl = "http://47.97.158.227:9090";
 const websocketUrl = "ws://47.97.158.227:9090";
