d4beea616bcc6e8f5a36497e5f393dca509f669d
ZSSXL <1271130458@qq.com>
2020-01-14 11:20:39 +0800

    再一次修改

--- ArrangeProduceClient/js/app-url.js
+++ ArrangeProduceClient/js/app-url.js
@@ -1,5 +1,6 @@
-/*const serverUrl = "http://192.168.1.110:8080";
-const websocketUrl = "ws://192.168.1.110:8080";*/
-
 const serverUrl = "http://47.97.158.227:9090";
 const websocketUrl = "ws://47.97.158.227:9090";
+
+/*
+const serverUrl = "http://192.168.1.108:8080";
+const websocketUrl = "ws://192.168.1.108:8080";*/
